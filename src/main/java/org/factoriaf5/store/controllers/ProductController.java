1ed67c95d10e47f0f75ff060c804cb2837c67bb0
Sonia Mestre <mestre.sonia@gmail.com>
2021-05-17 11:37:09 +0200

    fase 2

---
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController<health, get> {
+   @GetMapping ("/health")
+        public String check() {
+        return "hello, I'm a server";
+            };
+};
+
