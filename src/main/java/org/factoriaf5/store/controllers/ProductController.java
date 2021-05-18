11d57ff6608dd3fc869504b1ea2d8dfb7722c93e
Sonia Mestre <mestre.sonia@gmail.com>
2021-05-18 15:25:27 +0200

    inicio fase6

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -2,6 +2,7 @@ package org.factoriaf5.store.controllers;
 
 import org.factoriaf5.store.model.Product;
 import org.factoriaf5.store.models.Person;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class ProductController {
 
     public List<Product> products = new ArrayList<>();
+    private Object name;
 
     @GetMapping("/products")
     public List<Product> allProducts() {
@@ -22,6 +24,19 @@ public class ProductController {
     @PostMapping("/products")
     public void addProduct(Product product) {
         products.add(product);
+    }
+    @DeleteMapping(/DELETE/products{name})
+    public Product findProduct(String name, List<Product> products) {
+
+        Product[] product;
+        for (Product product : product) {
+            if (product.getName().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+
+        products.remove(product)
     }
 }
 
