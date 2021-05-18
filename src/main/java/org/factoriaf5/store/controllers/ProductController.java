df736e6d188803edc11c5d6b0e5b1226f3db7007
Sonia Mestre <mestre.sonia@gmail.com>
2021-05-18 14:46:35 +0200

    fase5

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,13 +1,29 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
+import org.factoriaf5.store.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-public class ProductController<health, get> {
-   @GetMapping ("/health")
-        public String check() {
-        return "hello, I'm a server";
-            };
-};
+public class ProductController {
+
+    public List<Product> products = new ArrayList<>();
+
+    @GetMapping("/products")
+    public List<Product> allProducts() {
+        return products;
+    }
+
+    @PostMapping("/products")
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+}
+
+
 
---
+++ src/main/java/org/factoriaf5/store/models/Person.java
@@ -0,0 +1,10 @@
+package org.factoriaf5.store.models;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+
+        this.name = name;
+    }
+}
